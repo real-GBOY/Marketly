03e13d0bc1f4c4a3c212cb8fdd6856b81268b6c2
real-GBOY <mahmmoudnayel2004@gmail.com>
2026-03-30 18:30:03 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
